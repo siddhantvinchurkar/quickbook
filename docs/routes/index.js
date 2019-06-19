75e8be6ec75c786450c160a1c47ebc8b03a4b553
Siddhant <siddhantvinchurkar@gmail.com>
2019-06-19 17:46:26 +0530

    Stuff

--- docs/routes/index.js
+++ docs/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+const admin = require('firebase-admin');
+let serviceAccount = require('serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+let db = admin.firestore();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Quickbook Admin' });
 });
 
 module.exports = router;
